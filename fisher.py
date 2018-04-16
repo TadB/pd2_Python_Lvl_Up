f02b411b2958f45e212eb3ee38b645c207a48ef0
TadB <tadeusz.brzytwa@gmail.com>
2018-04-16 23:40:12 +0200

    implementacja metody /logout

--- fisher.py
+++ fisher.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, session, request, render_template, make_response
+from flask import Flask, redirect, session, request, render_template, make_response, flash
 
 users = { 'Akwarysta69': 'J3si07r'}
 
@@ -12,14 +12,17 @@ def welcome():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    user=request.authorization['username']
-    if 'user' in session:
-        return redirect('/hello')
-    elif user in users and request.authorization['password']==users[user]:
-        session['user']=user
-        return 'loged in, welcome'
+    if request.method=='POST':
+        user=request.authorization['username']
+        if 'user' in session:
+            return redirect('/hello')
+        elif user in users and request.authorization['password']==users[user]:
+            session['user']=user
+            return 'loged in, welcome'
+        else:
+            return 'not loged in sory bro'
     else:
-        return 'not loged in sory bro'
+        return 'jestes niezalogowany'
 
 @app.route("/hello", methods=['GET'])
 def hello():
@@ -31,6 +34,16 @@ def hello():
         )
     else:
         return redirect('/login')
+
+@app.route("/logout", methods=['POST']) 
+def logout(): 
+    if 'user' in session:
+        session.pop('user', None)
+        flash('zostales wylogowany')
+        return redirect('/')
+    else:
+        return redirect('/login')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
